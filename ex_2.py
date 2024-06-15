50bcc29a64024e5ae1329111df26b6cb603991a7
EC2 Default User <ec2-user@ip-172-31-21-0.ec2.internal>
2024-06-15 19:40:51 +0000

    add ex_2.py

---
+++ ex_2.py
@@ -0,0 +1,12 @@
+# Create a tuple called names that contains several pet names. For instance:
+
+"""
+Name
+Asta
+Butterscotch
+Pudding
+Neptune
+Darwin
+"""
+
+names = ('Asta', 'Butterscotch', 'Pudding', 'Neptune', 'Darwin')
