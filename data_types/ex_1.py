e6d4bb42c3b77f7728f9021fd4237edf08ad53d7
EC2 Default User <ec2-user@ip-172-31-21-0.ec2.internal>
2024-06-15 19:32:28 +0000

    add data_types folder and 1st exercise

---
+++ data_types/ex_1.py
@@ -0,0 +1,14 @@
+# Identify the data type or class for each of the following values:
+"""
+VALUE           TYPE            CLASS
+"""
+'True'          # String        str
+False           # Boolean       bool
+(1, 2, 3)       # Tuple         tuple
+1.5             # Float         float
+[1, 2, 3]       # List          list  
+2               # Integer       int
+range(5)        # Range         range
+{1, 2, 3}       # Set           set
+None            # NoneType      NoneType
+{'foo': 'bar'}  # Dictionary    dict
