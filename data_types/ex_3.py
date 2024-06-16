269034982a1d521921269a3501ece09421b22183
EC2 Default User <ec2-user@ip-172-31-21-0.ec2.internal>
2024-06-16 18:15:48 +0000

    add ex_3.py to data_types

---
+++ data_types/ex_3.py
@@ -0,0 +1,18 @@
+# Create a dictionary named pets that contains a list of pet names and the type of animal. For instance:
+
+"""
+Name            Animal
+Asta            dog
+Butterscotch    cat
+Pudding         cat
+Neptune         fish
+Darwin          lizard
+"""
+
+pets = {
+        "Asta": "dog",
+        "Butterscotch": "cat",
+        "Pudding": "cat",
+        "Neptune": "fish",
+        "Darwin": "lizard",
+}
